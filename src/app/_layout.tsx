968449aa9ab1aa310287130d6b4ba31ae303b603
forimbajosh21 <forimbajosh21@gmail.com>
2025-05-23 07:39:17 +0800

    chore: create routing, Map screen, functionality

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,37 @@
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import useLocation from '../hooks/useLocation';
 
 export default function RootLayout() {
-  return <Stack />;
+  const { getCurrentLocationAndShowAlert } = useLocation();
+
+  useEffect(
+    () => {
+      getCurrentLocationAndShowAlert();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  return (
+    <GestureHandlerRootView style={styles.root}>
+      <BottomSheetModalProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ title: '', headerTransparent: true }}
+          />
+        </Stack>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,93 @@
-import { Text, View } from 'react-native';
+import { BottomSheetModal as GorhomBottomSheetModal } from '@gorhom/bottom-sheet';
+import { useEffect, useRef } from 'react';
+import { AppState, StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function Index() {
+import { useLocationStore, useMapStore } from '@/src/stores';
+
+import BottomSheetPlaceInfo from '@/src/components/BottomSheetPlaceInfo';
+import MapTools from '@/src/components/MapTools';
+import NotificationMapPermission from '@/src/components/NotificationMapPermission';
+import ThemedMapView from '@/src/components/ThemedMapView';
+import useLocation from '@/src/hooks/useLocation';
+
+export default function App() {
+  const { getCurrentLocation } = useLocation();
+
+  const permission = useLocationStore((state) => state.permission);
+  const location = useLocationStore((state) => state.location);
+  const isUserFromSettings = useLocationStore(
+    (state) => state.isUserFromSettings,
+  );
+
+  const setPoi = useMapStore((state) => state.setPoi);
+
+  const appState = useRef(AppState.currentState);
+  const mapRef = useRef<MapView>(null);
+  const placeInfoRef = useRef<GorhomBottomSheetModal>(null);
+
+  /** trigger getting of user location when the app goes from background to foreground */
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active' &&
+        isUserFromSettings
+      ) {
+        getCurrentLocation();
+      }
+
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, [getCurrentLocation, isUserFromSettings]);
+
+  /** force the map to re-center and animate to user coordinates */
+  function handleAnimateToUserLocation() {
+    if (permission === 'granted' && location) {
+      mapRef.current?.animateCamera({
+        center: {
+          latitude: location.latitude,
+          longitude: location.longitude,
+        },
+        zoom: 17,
+      });
+    }
+  }
+
+  function handleShowPlaceInfoSheet() {
+    placeInfoRef.current?.present();
+  }
+
+  function handleOnPlaceInfoDismiss() {
+    setPoi(null);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <NotificationMapPermission />
+      <MapTools onCenterMap={handleAnimateToUserLocation} />
+      <ThemedMapView
+        ref={mapRef}
+        onAnimateToUserLocation={handleAnimateToUserLocation}
+        onSelectPoi={handleShowPlaceInfoSheet}
+      />
+      <BottomSheetPlaceInfo
+        enableDismissOnClose
+        enablePanDownToClose
+        ref={placeInfoRef}
+        onDismiss={handleOnPlaceInfoDismiss}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    flex: 1,
+  },
+});
