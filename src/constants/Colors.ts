f82596d28372abba9ceb609540c10da995068c77
forimbajosh21 <forimbajosh21@gmail.com>
2025-05-23 07:34:33 +0800

    chore: setup theme configuration

---
+++ src/constants/Colors.ts
@@ -0,0 +1,16 @@
+export const Colors = {
+  light: {
+    backgroundPrimary: '#FFFFFF',
+    backgroundSecondary: '#F6F6F6', // F2F2F7
+    textPrimary: '#1C1C1E',
+    textSecondary: '#8E8E93',
+    active: '#007AFF',
+  },
+  dark: {
+    backgroundPrimary: '#2c2c2e',
+    backgroundSecondary: '#1C1C1E',
+    textPrimary: '#ffffff',
+    textSecondary: '#d1d1d6',
+    active: '#007AFF',
+  },
+};
